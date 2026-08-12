58ec3060e603cd4a16dfbf76c3da5783c52c41fe
simon <idahecho403@gmail.com>
2026-08-12 18:19:49 +0100

    Initialize database on Vercel

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carrental.settings")
 
+from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
+
 application = get_wsgi_application()
+
+call_command("migrate", interactive=False, verbosity=0)
